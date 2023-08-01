0dec7433bbcc3591f14c35efcef17fa3bbb6dbd2
MaurizioRus <mauriziocovelli9@gmail.com>
2023-08-01 17:59:18 +0200

    first

---
+++ esIfElse/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Maurizio";
+
+        if(myName.length() % 2 == 0) System.out.print("Even");
+
+        if(myName.length() % 2 != 0) System.out.print("Odd");
+    }
+}
